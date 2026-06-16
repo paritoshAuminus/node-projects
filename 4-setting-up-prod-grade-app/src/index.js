02004c2a3134ce63c5fcedccc80b342c00467caa
paritoshAuminus <paritoshverma2038@gmail.com>
2026-06-16 10:25:04 +0530

    Connected with db

---
+++ 4-setting-up-prod-grade-app/src/index.js
@@ -0,0 +1,35 @@
+// require("dotenv").config({path: "./env"}) // Old syntax (commonJS)
+import dotenv from 'dotenv';
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import express from "express";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error :: app.om ::", error)
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        }) 
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+})()
+*/
