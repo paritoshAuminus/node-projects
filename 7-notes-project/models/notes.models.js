42a32cf358b0d70b8a911e63c83f10f1748004f8
paritoshAuminus <paritoshverma2038@gmail.com>
2026-07-17 03:08:16 +0530

    Updated models with options

--- 7-notes-project/models/notes.models.js
+++ 7-notes-project/models/notes.models.js
@@ -1,8 +1,14 @@
 import mongoose, {Schema} from "mongoose";
 
 const notesSchema = new Schema({
-    title: String,
-    body: String
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
 }, {timestamps: true})
 
 export const Notes = mongoose.model("Notes", notesSchema)
